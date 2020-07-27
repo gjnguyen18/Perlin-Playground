69a649d7b887ba57dddce43251303f78ec93774c
Gia-Phong Nguyen <gnguyen@wisc.edu>
2020-07-26 18:57:07 -0700

    added blocklike mesh

--- js/displays/perlin2DTerrainBlocky.js
+++ js/displays/perlin2DTerrainBlocky.js
@@ -1,33 +1,17 @@
-
 import * as T from "../../libs/CS559-THREE/build/three.module.js";
 import { onWindowOnload } from "../../libs/helpers.js";
 import { perlinNoiseGenerator2D } from "./../noiseGenerators/perlinNoiseGenerator2D.js";
 
-const numSquares = 50;
-const amplitude = 180;
+const numSquares = 100;
+const amplitude = 80;
 const terrainSize = 200;
-
-let getQuadUV = (x, y, width, height, scaleX = numSquares, scaleY = numSquares) => {
-    return [
-        [
-            new T.Vector2(x / scaleX, y / scaleY),
-            new T.Vector2((x + width) / scaleX, (y + height) / scaleY),
-            new T.Vector2((x + width) / scaleX, y / scaleY)
-        ],
-        [
-            new T.Vector2(x / scaleX, y / scaleY),
-            new T.Vector2(x / scaleX, (y + height) / scaleY),
-            new T.Vector2((x + width) / scaleX, (y + height) / scaleY)
-        ]
-    ]
-}
 
 function drawPerlin2DTerrain() {
 
     // create surface points
     let perlinNoiseGenerator = new perlinNoiseGenerator2D();
 
-    perlinNoiseGenerator.setScale(0.06);
+    perlinNoiseGenerator.setScale(0.04);
     perlinNoiseGenerator.setOctaves(4);
 
     let meshPoints = [];
@@ -35,15 +19,15 @@ function drawPerlin2DTerrain() {
         let column = [];
         for(let k=0; k<numSquares+1; k++) {
             let result = perlinNoiseGenerator.getVal(i, k);
-            let val = Math.floor(result * 20) / 20;
-            column.push(result*amplitude);
+            let val = Math.floor(result * 50) / 50;
+            column.push(val*amplitude);
         }
         meshPoints.push(column);
     }
 
     // create canvas  
     let canvas = /** @type {HTMLCanvasElement} */ (document.getElementById(
-        "terrainPerlin2DCanvas"
+        "blockyTerrainPerlin2DCanvas"
     ));
     
     // Set up the renderer, which will create the Canvas for us
@@ -63,30 +47,57 @@ function drawPerlin2DTerrain() {
     let geometry = new T.Geometry();
     let material = new T.MeshPhongMaterial({ color: "lightblue" });
 
-    for(let i=0; i<numSquares+1; i++) {
-        for(let k=0; k<numSquares+1; k++) {
-            geometry.vertices.push(new T.Vector3(k*terrainSize/numSquares, meshPoints[i][k], i*terrainSize/numSquares));
-        }
+    let drawSquare = (p1, p2, p3, p4) => {
+        geometry.faces.push(new T.Face3(p4, p2, p1));
+        geometry.faces.push(new T.Face3(p3, p4, p1));
     }
 
     for(let i=0; i<numSquares; i++) {
         for(let k=0; k<numSquares; k++) {
-            geometry.faces.push(new T.Face3(i*(numSquares+1)+k, (i+1)*(numSquares+1)+k+1, i*(numSquares+1)+k+1));
-            geometry.faces.push(new T.Face3(i*(numSquares+1)+k, (i+1)*(numSquares+1)+k, (i+1)*(numSquares+1)+k+1));
+            let thisVal = meshPoints[k][i];
+            geometry.vertices.push(new T.Vector3(k*terrainSize/numSquares, thisVal, i*terrainSize/numSquares));
+            geometry.vertices.push(new T.Vector3((k+1)*terrainSize/numSquares, thisVal, i*terrainSize/numSquares));
+            geometry.vertices.push(new T.Vector3(k*terrainSize/numSquares, thisVal, (i+1)*terrainSize/numSquares));
+            geometry.vertices.push(new T.Vector3((k+1)*terrainSize/numSquares, thisVal, (i+1)*terrainSize/numSquares));
+
+            let l = geometry.vertices.length;
+            // geometry.faces.push(new T.Face3(vLength-1, vLength-3, vLength-4));
+            // geometry.faces.push(new T.Face3(vLength-2, vLength-1, vLength-4));
+            drawSquare(l-4,l-3,l-2,l-1);
+
+            if(k>0) {
+                if(meshPoints[k-1][i] < thisVal) {
+                    drawSquare(l-5,l-7,l-2,l-4);
+                }
+                else if(meshPoints[k-1][i] > thisVal) {
+                    drawSquare(l-4,l-2,l-7,l-5);
+                }
+            }
+
+            if(i>0) {
+                let iStep = numSquares * 4;
+                if(meshPoints[k][i-1] < thisVal) {
+                    drawSquare(l-2-iStep,l-1-iStep,l-4,l-3);
+                }
+                else if(meshPoints[k][i-1] > thisVal) {
+                    drawSquare(l-3, l-4, l-1-iStep, l-2-iStep);
+                }
+            }
         }
     }
+
+
+
+    // for(let i=0; i<numSquares; i++) {
+    //     for(let k=0; k<numSquares; k++) {
+    //         geometry.faces.push(new T.Face3(i*(numSquares+1)+k, (i+1)*(numSquares+1)+k+1, i*(numSquares+1)+k+1));
+    //         geometry.faces.push(new T.Face3(i*(numSquares+1)+k, (i+1)*(numSquares+1)+k, (i+1)*(numSquares+1)+k+1));
+    //     }
+    // }
 
     geometry.faceVertexUvs = [[]];
     geometry.computeFaceNormals();
     geometry.uvsNeedUpdate = true;
-
-    for(let i=0; i<numSquares; i++) {
-        for(let k=0; k<numSquares; k++) {
-            let face = getQuadUV(k, i, 1, 1);
-            geometry.faceVertexUvs[0].push(face[0]);
-            geometry.faceVertexUvs[0].push(face[1]);
-        }
-    }
 
     let terrain = new T.Mesh(geometry, material);
 
@@ -102,6 +113,11 @@ function drawPerlin2DTerrain() {
     camera.position.y = 160;
     camera.position.x = -120;
     camera.lookAt(0,-terrainSize*.15,0);
+
+    // renderer.shadowMapEnabled = true;
+    // pointLight.castShadow = true;
+    // terrain.castShadow = true;
+    // terrain.receiveShadow = true;
 
     renderer.render(scene, camera);
   
