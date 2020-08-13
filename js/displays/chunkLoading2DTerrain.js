9a762501025642d6c4252f2e77e96ec2fa5d8a40
Gia-Phong Nguyen <gnguyen@wisc.edu>
2020-08-12 17:39:59 -0700

    added sliders to terrain chunk loading

--- js/displays/chunkLoading2DTerrain.js
+++ js/displays/chunkLoading2DTerrain.js
@@ -3,15 +3,11 @@ import { OrbitControls } from "../../libs/CS559-THREE/examples/jsm/controls/Orbi
 import { onWindowOnload, createSlider, createCheckbox } from "../tools/helpers.js";
 import { PerlinNoiseGenerator2D } from "../noiseGenerators/perlinNoiseGenerator2D.js";
 
-var size = 100;
 var amplitude = 500;
 var scale = 0.014;
 var octaves = 7;
-var res = 2;
-var resOptions = [25, 50, 100, 200, 400];
 var chunkSize = 16;
 var spaceBetweenVertices = 2;
-var autoAdjust = true;
 var addChunkRange = 8;
 var deleteChunkRange = 12;
 var moveSpeed = 4;
@@ -31,13 +27,13 @@ function drawPerlin2DTerrain() {
         "chunkLoading2DTerrainCanvas"
     ));
     let renderer = new T.WebGLRenderer({ canvas: canvas });
-    let camera = new T.PerspectiveCamera(50, 1, 0.1, 2000);
+    let camera = new T.PerspectiveCamera(50, 1, 0.1, 4000);
     let scene = new T.Scene(); 
 
     // createLights
     let ambientLight = new T.AmbientLight(0xffffff, 0.25);
     scene.add(ambientLight);
-    let pointLight = new T.PointLight(0xffffff, 1, 2000);
+    let pointLight = new T.PointLight(0xffffff, 1, 4000);
     pointLight.position.set(chunkSize * spaceBetweenVertices * 0.75, 200, 0);
     scene.add(pointLight);
 
@@ -45,7 +41,6 @@ function drawPerlin2DTerrain() {
         let geometry = new T.Geometry();
         let material = new T.MeshLambertMaterial({ color: "lightblue" });
 
-        // add vertices to geometry
         for(let i = x * chunkSize; i < x * chunkSize + chunkSize + 1; i++) {
             for(let k = y * chunkSize; k < y * chunkSize + chunkSize + 1; k++) {
                 let result = perlinNoiseGenerator.getVal(i, k) * amplitude;
@@ -53,7 +48,6 @@ function drawPerlin2DTerrain() {
             }
         }
 
-        // add faces to geometry
         for(let i=0; i<chunkSize; i++) {
             for(let k=0; k<chunkSize; k++) {
                 geometry.faces.push(new T.Face3(i*(chunkSize+1)+k+1, (i+1)*(chunkSize+1)+k+1, i*(chunkSize+1)+k));
@@ -61,44 +55,32 @@ function drawPerlin2DTerrain() {
             }
         }
 
-        // console.log(geometry);
         geometry.computeFaceNormals();
 
         let terrain = new T.Mesh(geometry, material);
-        // let terrainGroup = new T.Group();
-        // terrainGroup.add(terrain);
-        // terrain.position.x = -TERRAIN_SIZE/2;
-        // terrain.position.z = -TERRAIN_SIZE/2;
-        // terrain.position.y = -amplitude/2;
         return terrain;
     }
 
-    // let terrainGroup = new T.Group();
-    // scene.add(terrainGroup);
-    // let terrainMeshes = new Map();
-
     let getPlayerChunk = () => {
-        let spacing = length/size;
         return [
             Math.floor(playerPosition[0]/(spaceBetweenVertices*chunkSize)),  
             Math.floor(playerPosition[1]/(spaceBetweenVertices*chunkSize))
         ];
     }
 
-    let updateChunks = (currChunk, closeRange, farRange) => {
-        for(let i=currChunk[0]-closeRange; i<=currChunk[0]+closeRange; i++) {
-            for(let k=currChunk[1]-closeRange; k<=currChunk[1]+closeRange; k++) {
+    let updateChunks = (currChunk) => {
+        for(let i=currChunk[0]-addChunkRange; i<=currChunk[0]+addChunkRange; i++) {
+            for(let k=currChunk[1]-addChunkRange; k<=currChunk[1]+addChunkRange; k++) {
                 let distanceToPlayerChunk = Math.sqrt((currChunk[0] - i) * (currChunk[0] - i) + (currChunk[1] - k) * (currChunk[1] - k));
-                if(distanceToPlayerChunk < closeRange && !nearChunks.has([i, k].toString())) {
+                if(distanceToPlayerChunk < addChunkRange && !nearChunks.has([i, k].toString())) {
                     nearChunks.set([i, k].toString(), [i, k]);
                 }
             }
         }
         nearChunks.forEach(function(value, key) {
             let distanceToPlayerChunk = Math.sqrt((currChunk[0] - value[0]) * (currChunk[0] - value[0]) + (currChunk[1] - value[1]) * (currChunk[1] - value[1]));
-            if(distanceToPlayerChunk >= farRange) {
+            if(distanceToPlayerChunk >= deleteChunkRange) {
                 nearChunks.delete(value.toString());
-                // terrainGroup.remove(terrainGroup.children[prevNearChunks.get(value.toString())]);
                 let mesh = prevNearChunks.get(value.toString());
                 scene.remove(mesh);
                 prevNearChunks.delete(value.toString());
@@ -107,41 +89,39 @@ function drawPerlin2DTerrain() {
     }
 
     let updateTerrain = () => {
-        perlinNoiseGenerator.setScale(scale);
-        perlinNoiseGenerator.setOctaves(octaves);
-
         let playerChunk = getPlayerChunk();
-        updateChunks(playerChunk, addChunkRange, deleteChunkRange, nearChunks);
+        updateChunks(playerChunk);
         nearChunks.forEach(function(value, key) {
             if(!prevNearChunks.has(key)) {
-                let mesh = createChunkMesh(value[0], value[1]);
+                let mesh = createChunkMesh(value[0], value[1], chunkSize);
                 prevNearChunks.set(key, mesh);
-                // console.log(terrainGroup.children.length);
-                // terrainMeshes.add(mesh)
                 scene.add(mesh);
             }
         });
-
-        // scene.add(mesh);
-
-        // terrainGroup.position.x = chunkSize*2.5;
-        // terrainGroup.position.z = -chunkSize*20;
-
-        // renderer.shadowMap.enabled = true;
-        // renderer.shadowMap.type = T.PCFSoftShadowMap;
-        // scene.children[1].castShadow = true;
-        // terrain.castShadow = true;
-        // terrain.receiveShadow = true;
-
-        // renderer.render(scene, camera);
     }
+
+    let clearTerrain = () => {
+        nearChunks.forEach(function(value, key) {
+            let mesh = prevNearChunks.get(value.toString());
+            scene.remove(mesh);
+            nearChunks.delete(value.toString());
+            prevNearChunks.delete(value.toString());
+        });
+    }
+
+    let recreateTerrain = () => {
+        perlinNoiseGenerator.setScale(scale);
+        perlinNoiseGenerator.setOctaves(octaves);
+        clearTerrain();
+        updateTerrain();
+    }
+    recreateTerrain();
 
     let updateCamera = () => {
         camera.position.set(playerPosition[0], cameraDistance*1.5, playerPosition[1] + cameraDistance*1.2);
         pointLight.position.set(playerPosition[0] + cameraDistance * 1.2, cameraDistance * .6, playerPosition[1] + cameraDistance * 1.2);
         camera.lookAt(playerPosition[0], 200, playerPosition[1]);
     }
-    // let controls = new OrbitControls(camera, renderer.domElement);
     
     
 
@@ -152,7 +132,6 @@ function drawPerlin2DTerrain() {
     let keyMap = new Map();
     let activateKey = (e) => {
         keyMap.set(e.key, true)
-        // movePlayer();
     }
     let deactivateKey = (e) => {
         keyMap.set(e.key, false)
@@ -184,6 +163,14 @@ function drawPerlin2DTerrain() {
     let seedBox = /** @type {HTMLInputElement} */ (document.getElementById("seedBox"));
     let seedWarning = /** @type {HTMLInputElement} */ (document.getElementById("seedWarning"));
 
+    let cameraDistanceSlider = createSlider("Camera Distance", 100, 2000, 10, cameraDistance);
+    let spaceBetweenVerticesSlider = createSlider("Space Between Vertices", 1, 20, 1, spaceBetweenVertices);
+    let chunkSizeSlider = createSlider("Chunk Size", 1, 40, 1, chunkSize);
+    let chunkRangeSlider = createSlider("Chunk Range", 2, 32, 1, addChunkRange);
+    let scaleSlider = createSlider("Scale", 0.0001, 0.4, 0.0001, scale);
+    let octavesSlider = createSlider("Octaves", 1, 10, 1, octaves);
+    let amplitudeSlider = createSlider("Amplitude", 0, 800, 1, amplitude);
+
     seedBox.value = seed;
     seedBox.onchange = () => {
         if(Number(seedBox.value) < 0 || Number(seedBox.value) > 999999999) {
@@ -194,9 +181,63 @@ function drawPerlin2DTerrain() {
             perlinNoiseGenerator = new PerlinNoiseGenerator2D(seed);
             perlinNoiseGenerator.setScale(Number(scaleSlider.value));
             perlinNoiseGenerator.setOctaves(Number(octavesSlider.value));
-            createTerrain();
+            recreateTerrain();
             seedWarning.innerHTML = "";
         }
+    }
+
+    cameraDistanceSlider[0].oninput = () => {
+        cameraDistanceSlider[1].innerHTML = "Camera Distance: " + cameraDistanceSlider[0].value;
+        cameraDistance = cameraDistanceSlider[0].value;
+    }
+
+    spaceBetweenVerticesSlider[0].oninput = () => {
+        spaceBetweenVerticesSlider[1].innerHTML = "Space Between Vertices: " + spaceBetweenVerticesSlider[0].value;
+    }
+    spaceBetweenVerticesSlider[0].onchange = () => {
+        spaceBetweenVertices = spaceBetweenVerticesSlider[0].value;
+        recreateTerrain();
+    }
+
+    chunkSizeSlider[0].oninput = () => {
+        chunkSizeSlider[1].innerHTML = "Chunk Size: " + chunkSizeSlider[0].value;
+    }
+    chunkSizeSlider[0].onchange = () => {
+        chunkSize = Number(chunkSizeSlider[0].value);
+        recreateTerrain();
+    }
+
+    chunkRangeSlider[0].oninput = () => {
+        chunkRangeSlider[1].innerHTML = "Chunk Range: " + chunkRangeSlider[0].value;
+    }
+    chunkRangeSlider[0].onchange = () => {
+        addChunkRange = Number(chunkRangeSlider[0].value);
+        deleteChunkRange = addChunkRange + 4;
+        recreateTerrain();
+    }
+
+    scaleSlider[0].oninput = () => {
+        scaleSlider[1].innerHTML = "Scale: " + scaleSlider[0].value;
+    }
+    scaleSlider[0].onchange = () => {
+        scale = scaleSlider[0].value;
+        recreateTerrain();
+    }
+
+    octavesSlider[0].oninput = () => {
+        octavesSlider[1].innerHTML = "Octaves: " + octavesSlider[0].value;
+    }
+    octavesSlider[0].onchange = () => {
+        octaves = octavesSlider[0].value;
+        recreateTerrain();
+    }
+    
+    amplitudeSlider[0].oninput = () => {
+        amplitudeSlider[1].innerHTML = "Amplitude: " + amplitudeSlider[0].value;
+    }
+    amplitudeSlider[0].onchange = () => {
+        amplitude = amplitudeSlider[0].value;
+        recreateTerrain();
     }
 
     function animate() {
